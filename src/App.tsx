7473a3cad6851f7ffe652e4a999e9a04e0b0814d
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-09-13 13:23:42 +0800

    feat: add graphql fix styling add element use example in react add hooks useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,54 @@
+import { gql, useQuery } from "@apollo/client";
+import { Fragment } from "react";
 import "./App.css";
 
+const graphQLQuery = gql`
+  {
+    capsules(limit: 10) {
+      dragon {
+        crew_capacity
+        description
+        diameter {
+          meters
+        }
+      }
+      id
+      landings
+      missions {
+        name
+        flight
+      }
+    }
+  }
+`;
+
 function App() {
-  return <div>Hello, world</div>;
+  const { loading, error, data } = useQuery(graphQLQuery);
+  console.log(data);
+  if (!loading && data)
+    return (
+      <>
+        <div className="capsules">
+          {data.capsules.map((capsule: any, capsuleI: number) => (
+            <div key={capsuleI}>
+              <h1>Dragon</h1>
+              <p>{capsule.dragon.description}</p>
+              <h4>Capsule missions:</h4>
+              {capsule.missions.map((mission: any, missionI: number) => (
+                <Fragment key={missionI}>{mission.name}</Fragment>
+              ))}
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  if (error)
+    return (
+      <>
+        <div>An Error occurred.</div>
+      </>
+    );
+  return <div>Loading...</div>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
+);
